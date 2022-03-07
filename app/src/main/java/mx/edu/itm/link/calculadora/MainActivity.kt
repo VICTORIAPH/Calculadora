4de22df867474d8f0111aa6a731983a26dccae05
Vickyyyyyy <vicky454ph@gmail.com>
2022-03-06 23:00:35 -0600

    posision y color de btns

--- app/src/main/java/mx/edu/itm/link/calculadora/MainActivity.kt
+++ app/src/main/java/mx/edu/itm/link/calculadora/MainActivity.kt
@@ -2,10 +2,15 @@ package mx.edu.itm.link.calculadora
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import mx.edu.itm.link.calculadora.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
     }
 }
